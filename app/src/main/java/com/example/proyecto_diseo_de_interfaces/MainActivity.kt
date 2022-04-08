51bb21519cc678bd90d73d8eb5356a0cda3e1e89
alcss <fernandoalcalacasas@hotmail.com>
2022-04-08 16:54:22 -0500

    Dejado Kotlin de lado, Profundizando más en desarrollo de apps

--- app/src/main/java/com/example/proyecto_diseo_de_interfaces/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_diseo_de_interfaces/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.proyecto_diseo_de_interfaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.proyecto_diseo_de_interfaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        setContentView(R.layout.pantallita)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener {
+            checkMail()
+        }
+
+    }
+    fun checkMail(){
+        if(binding.textBoxUser.text.isNotEmpty()){
+            //COOL
+        }else{
+            showError()
+        }
+    }
+    fun showError(){
+        Toast.makeText(this, "El nombre del usuario está vacío, ", Toast.LENGTH_SHORT).show()
     }
 }
