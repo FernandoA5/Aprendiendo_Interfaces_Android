971215331a6329e38fb4dbe0178e8d47939fda5d
alcss <fernandoalcalacasas@hotmail.com>
2022-04-09 12:56:39 -0500

    Comenzando el render del viewHolder

--- app/src/main/java/com/example/proyecto_diseo_de_interfaces/adapter/SuperHeroAdapter.kt
+++ app/src/main/java/com/example/proyecto_diseo_de_interfaces/adapter/SuperHeroAdapter.kt
@@ -1,5 +1,21 @@
 package com.example.proyecto_diseo_de_interfaces.adapter
 
-class SuperHeroAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.proyecto_diseo_de_interfaces.R
+import com.example.proyecto_diseo_de_interfaces.SuperHero
+
+class SuperHeroAdapter(private val superheroList:List<SuperHero>) : RecyclerView.Adapter<SuperHeroViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperHeroViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return SuperHeroViewHolder(layoutInflater.inflate(R.layout.item_superhero, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: SuperHeroViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int = superheroList.size
 
 }
--- app/src/main/java/com/example/proyecto_diseo_de_interfaces/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/proyecto_diseo_de_interfaces/adapter/SuperHeroViewHolder.kt
@@ -1,5 +1,12 @@
 package com.example.proyecto_diseo_de_interfaces.adapter
 
-class SuperHeroViewHolder {
-    
+import android.view.LayoutInflater
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.proyecto_diseo_de_interfaces.SuperHero
+
+class SuperHeroViewHolder(view:View):RecyclerView.ViewHolder(view) {
+    fun render(superHero: SuperHero){
+        
+    }
 }
