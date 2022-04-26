f59a13a8f8636997e86a21bf0b0be35f527f7aa0
alcss <fernandoalcalacasas@hotmail.com>
2022-04-26 00:41:58 -0500

    Item Decoration en el Recycler View aún en Lista

--- app/src/main/java/com/example/proyecto_diseo_de_interfaces/RecyclerSH.kt
+++ app/src/main/java/com/example/proyecto_diseo_de_interfaces/RecyclerSH.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.proyecto_diseo_de_interfaces.adapter.SuperHeroAdapter
 import com.example.proyecto_diseo_de_interfaces.databinding.ActivityRecyclerShBinding
@@ -21,11 +22,14 @@ class RecyclerSH : AppCompatActivity() {
         initRecyclerView()
     }
     fun initRecyclerView(){
+        val manager = LinearLayoutManager(this)
+        val decoration= DividerItemDecoration(this, manager.orientation)
         val recyclerView = binding.recyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = manager
         recyclerView.adapter = SuperHeroAdapter(SuperHeroProvider.superHeroList){
             superHero ->  onItemSelected(superHero)
         }
+        recyclerView.addItemDecoration(decoration)
     }
     fun onItemSelected(superHero: SuperHero){
         Toast.makeText(this, superHero.superHero, Toast.LENGTH_SHORT).show()
